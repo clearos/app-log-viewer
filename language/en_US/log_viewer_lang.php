9cb396abd79533461f7df76e87de6edf59ebed92
Ben Chambers <bchambers@clearfoundation.com>
2012-03-20 14:50:41 +0000

    - Updated description

--- language/en_US/log_viewer_lang.php
+++ language/en_US/log_viewer_lang.php
@@ -1,4 +1,4 @@
 <?php
 
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_app_description'] = 'Log Viewer description'; // FIXME
+$lang['log_viewer_app_description'] = 'Tabular display of all system log files.  Viewing and searching of log files are essential and typically the first step in troubleshooting problems with your server.';
