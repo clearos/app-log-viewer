dcbb662066f7531bdbe938bfb9145d98965b9dcd
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-15 01:46:26 +0000

    - Updated translations

--- language/pl_PL/log_viewer_lang.php
+++ language/pl_PL/log_viewer_lang.php
@@ -11,5 +11,5 @@ $lang['log_viewer_file'] = 'Plik dziennika';
 $lang['log_viewer_filter'] = 'Filtr';
 $lang['log_viewer_invalid_log_file'] = 'Nieprawidłowy plik dziennika.';
 $lang['log_viewer_logs'] = 'Logi';
-$lang['log_viewer_result_set_too_big'] = 'The result set is larger than the maximum that can be displayed.  Please use the filter to refine your results.';
+$lang['log_viewer_result_set_too_big'] = 'Wyświetlony wynik jest większy niż może zostać wyświetlony.  Użyj filtra aby uszczegółowić wynik.';
 $lang['log_viewer_show_full_line'] = 'Pokaż pełny wiersz';
