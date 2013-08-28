64b61a72db5c8132ab062270e09875d14ff64f92
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-28 13:55:30 +0000

    - Prepped for 6.5.0 Beta 1 release

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'log_viewer';
-$app['version'] = '1.4.20';
+$app['version'] = '1.5.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-log-viewer.spec
+++ packaging/app-log-viewer.spec
@@ -1,7 +1,7 @@
 
 Name: app-log-viewer
 Epoch: 1
-Version: 1.4.20
+Version: 1.5.0
 Release: 1%{dist}
 Summary: Log Viewer
 License: GPLv3
