b1b7f07a665725ca4bfc9559b72c278f20a7e272
pcbaldwin <pbaldwin@clearfoundation.com>
2015-04-28 06:12:01 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'log_viewer';
-$app['version'] = '2.0.20';
+$app['version'] = '2.0.24';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-log-viewer.spec
+++ packaging/app-log-viewer.spec
@@ -1,7 +1,7 @@
 
 Name: app-log-viewer
 Epoch: 1
-Version: 2.0.20
+Version: 2.0.24
 Release: 1%{dist}
 Summary: Log Viewer
 License: GPLv3
