84d137f8a35e3e438759e61e86a1f22de7e5397d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 04:33:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/log_viewer_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_name'] = 'Log Viewer';
+$lang['log_viewer_display'] = 'Display';
+$lang['log_viewer_entry'] = 'Log Entry';
+$lang['log_viewer_export_to_file'] = 'Export to File';
+$lang['log_viewer_file'] = 'Log File';
+$lang['log_viewer_filter'] = 'Filter';
+$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
+$lang['log_viewer_logs'] = 'Logs';
+$lang['log_viewer_result_set_too_big'] = ' ';
+$lang['log_viewer_show_full_line'] = 'Show Full Line';
