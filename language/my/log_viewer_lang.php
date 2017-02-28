ec06a91efd0d89af26d9b5db285a257e446a14d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:50:00 +0000

    Add new files and directories and commit changes to local repository

--- language/my/log_viewer_lang.php
+++ language/my/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'အဆိုပါ Log in ဝင်ရန် Viewer ကို app ကိုအားလုံး system ကိုမှတ်တမ်းဖိုင်တွေရဲ့ tabular display ကိုပေးစွမ်းသည်။ ကြည့်ရှုခြင်းနှင့်မှတ်တမ်းဖိုင်များကိုရှာဖွေနေမရှိမဖြစ်ဖြစ်ကြပြီးပုံမှန်အားဖြင့်သင့် system နှင့်အတူ troubleshooting ပြဿနာတွေအတွက်ပထမဦးဆုံးခြေလှမ်း။';
+$lang['log_viewer_app_name'] = 'log Viewer ကို';
+$lang['log_viewer_display'] = 'ပြသ';
+$lang['log_viewer_entry'] = 'log Entry &#39;';
+$lang['log_viewer_export_to_file'] = 'ဖိုင်မှပို့ကုန်';
+$lang['log_viewer_file'] = 'ဖိုင် Log';
+$lang['log_viewer_filter'] = 'ရေစစ်';
+$lang['log_viewer_invalid_log_file'] = 'မှားနေသောမှတ်တမ်းဖိုင်။';
+$lang['log_viewer_logs'] = 'သစ်လုံး';
+$lang['log_viewer_result_set_too_big'] = 'အဆိုပါရလဒ်အစုံဖော်ပြပေးနိုင်ပါတယ်အများဆုံးထက်ပိုကြီးဖြစ်ပါတယ်။ သင့်ရဲ့ရလဒ်တွေကိုသနျ့စဖို့အတွက် filter ကိုသုံးပေးပါ။';
+$lang['log_viewer_show_full_line'] = 'အပြည့်အဝလိုင်းပြရန်';
