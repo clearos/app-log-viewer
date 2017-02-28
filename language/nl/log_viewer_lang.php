a396df8e13840399b12159433654f97e368998b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:55:34 +0000

    Add new files and directories and commit changes to local repository

--- language/nl/log_viewer_lang.php
+++ language/nl/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'De Log Viewer app biedt tabel weergave van alle systeem logbestanden. Het bekijken en zoeken van logbestanden zijn essentieel en meestal de eerste stap in het oplossen van problemen met uw systeem.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
+$lang['log_viewer_display'] = 'tonen';
 $lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
+$lang['log_viewer_export_to_file'] = 'Exporteren naar bestand';
 $lang['log_viewer_file'] = 'Log File';
 $lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_invalid_log_file'] = 'Ongeldig logbestand.';
+$lang['log_viewer_logs'] = 'logs';
+$lang['log_viewer_result_set_too_big'] = 'Het resultaat set is groter dan het maximum dat kan worden weergegeven. Maak gebruik van de filter om uw resultaten te verfijnen.';
+$lang['log_viewer_show_full_line'] = 'Toon volledige lijn';
