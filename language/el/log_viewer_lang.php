9f900f4ab55c527f6aaec3af990c30eef8c5c16c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:51:43 +0000

    Add new files and directories and commit changes to local repository

--- language/el/log_viewer_lang.php
+++ language/el/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'Η εφαρμογή Log Viewer παρέχει ένα πίνακα εμφάνισης όλων των αρχείων καταγραφής του συστήματος. Προβολή και αναζήτηση των αρχείων καταγραφής είναι απαραίτητη και συνήθως το πρώτο βήμα για την αντιμετώπιση προβλημάτων με το σύστημά σας.';
+$lang['log_viewer_app_name'] = 'log Viewer';
+$lang['log_viewer_display'] = 'Απεικόνιση';
+$lang['log_viewer_entry'] = 'Είσοδος Είσοδος';
+$lang['log_viewer_export_to_file'] = 'Εξαγωγή σε αρχείο';
+$lang['log_viewer_file'] = 'συνδεθείτε αρχείου';
+$lang['log_viewer_filter'] = 'Φίλτρο';
+$lang['log_viewer_invalid_log_file'] = 'Μη έγκυρο αρχείο καταγραφής.';
+$lang['log_viewer_logs'] = 'logs';
+$lang['log_viewer_result_set_too_big'] = 'Το σύνολο των αποτελεσμάτων είναι μεγαλύτερο από το μέγιστο που μπορεί να εμφανιστούν. Παρακαλούμε χρησιμοποιήστε το φίλτρο για να βελτιώσετε τα αποτελέσματά σας.';
+$lang['log_viewer_show_full_line'] = 'Δείτε την πλήρη γραμμή';
