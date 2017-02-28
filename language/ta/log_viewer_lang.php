9ac9eb56a1484eec6f8cf484a87520e1e908880b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:58:11 +0000

    Add new files and directories and commit changes to local repository

--- language/ta/log_viewer_lang.php
+++ language/ta/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'பதிவு காட்டியின் பயன்பாட்டை அனைத்து அமைப்பு பதிவு கோப்புகளை அட்டவணை காட்சி அளிக்கிறது. காண்பதில் மற்றும் பதிவு கோப்புகளை தேடி அத்தியாவசிய மற்றும் உங்கள் கணினியில் பழுது நீக்கும் பிரச்சினைகள் பொதுவாக முதல் படிதான்.';
+$lang['log_viewer_app_name'] = 'பதிவு காட்டியின்';
+$lang['log_viewer_display'] = 'காட்சி';
+$lang['log_viewer_entry'] = 'பதிகையின்';
+$lang['log_viewer_export_to_file'] = 'ஏற்றுமதி கோப்பு';
+$lang['log_viewer_file'] = 'பதிவு கோப்பு';
+$lang['log_viewer_filter'] = 'வடிகட்டி';
+$lang['log_viewer_invalid_log_file'] = 'தவறான பதிவு கோப்பு.';
+$lang['log_viewer_logs'] = 'பதிவுகள்';
+$lang['log_viewer_result_set_too_big'] = 'விளைவாக தொகுப்பு காட்டப்படும் முடியும் என்று அதிகபட்ச விட பெரியதாகும். உங்கள் முடிவு சுத்தி வடிகட்டி பயன்படுத்தவும்.';
+$lang['log_viewer_show_full_line'] = 'முழு வரி காட்டு';
