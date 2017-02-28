939d2cc884186d967e0f1ffeb26295aa03aa9164
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:52:31 +0000

    Add new files and directories and commit changes to local repository

--- language/id/log_viewer_lang.php
+++ language/id/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'Log Viewer aplikasi memberikan tampilan tabel dari semua file sistem log. Melihat dan mencari file log yang penting dan biasanya langkah pertama dalam memecahkan masalah dengan sistem Anda.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_display'] = 'pameran';
+$lang['log_viewer_entry'] = 'Log Masuk';
+$lang['log_viewer_export_to_file'] = 'Ekspor ke File';
+$lang['log_viewer_file'] = 'log file';
+$lang['log_viewer_filter'] = 'Menyaring';
+$lang['log_viewer_invalid_log_file'] = 'file log tidak valid.';
+$lang['log_viewer_logs'] = 'log';
+$lang['log_viewer_result_set_too_big'] = 'Set hasil lebih besar dari maksimum yang dapat ditampilkan. Silakan gunakan filter untuk menyaring hasil.';
+$lang['log_viewer_show_full_line'] = 'Tampilkan Jalur Penuh';
