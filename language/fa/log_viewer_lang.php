f625adc3726201647c9a2eeb842feaafa84919eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:55:57 +0000

    Add new files and directories and commit changes to local repository

--- language/fa/log_viewer_lang.php
+++ language/fa/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'برنامه ورود Viewer یک صفحه نمایش جدولی از تمام فایل های سیستم ورود به سیستم فراهم می کند. مشاهده و جستجو از ورود فایل های ضروری و معمولا اولین گام در عیب یابی مشکلات با سیستم شما می باشد.';
+$lang['log_viewer_app_name'] = 'ورود Viewer';
+$lang['log_viewer_display'] = 'نمایش دادن';
+$lang['log_viewer_entry'] = 'ورود ورود به سیستم';
+$lang['log_viewer_export_to_file'] = 'صادرات به فایل';
+$lang['log_viewer_file'] = 'ورود به سیستم فایل';
+$lang['log_viewer_filter'] = 'صافی';
+$lang['log_viewer_invalid_log_file'] = 'ورود به سیستم فایل نامعتبر است.';
+$lang['log_viewer_logs'] = 'گزارش ها';
+$lang['log_viewer_result_set_too_big'] = 'مجموعه نتیجه بیش از حداکثر است که می تواند نمایش داده شود. لطفا از فیلتر استفاده کنید برای بهبود نتایج خود را.';
+$lang['log_viewer_show_full_line'] = 'نمایش خط کامل';
