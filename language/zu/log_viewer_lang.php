2d9eee5872ea730e1ba2b96dd8668632e12657c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:00:07 +0000

    Add new files and directories and commit changes to local repository

--- language/zu/log_viewer_lang.php
+++ language/zu/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'Uhlelo lokusebenza Log Viewer inikeza ukubonisa tabular wonke amafayela log system. Ukubukela kanye ukucinga amafayela log ibalulekile futhi ngokuvamile kuyisinyathelo sokuqala Ukuxazulula izinkinga nge system yakho.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_display'] = 'Khombisa';
+$lang['log_viewer_entry'] = 'okufakwayo kwelogi';
+$lang['log_viewer_export_to_file'] = 'Export Ifayela';
+$lang['log_viewer_file'] = 'Ngena Ifayela';
+$lang['log_viewer_filter'] = 'Hlunga';
+$lang['log_viewer_invalid_log_file'] = 'ifayela log engavumelekile.';
+$lang['log_viewer_logs'] = 'izingodo';
+$lang['log_viewer_result_set_too_big'] = 'Umphumela isethi kunenani esiphezulu okungatholakala ukuboniswa. Sicela usebenzise ukuhlunga imiphumela yakho.';
+$lang['log_viewer_show_full_line'] = 'Bonisa Line Full';
