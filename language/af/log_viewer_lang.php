07ba2f99015b18407a93f0464ec900dec9003991
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:48:48 +0000

    Add new files and directories and commit changes to local repository

--- language/af/log_viewer_lang.php
+++ language/af/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
+$lang['log_viewer_app_description'] = 'Die Meld Viewer app bied &#39;n tabel vertoon van alle stelsel log files. Kyk en soek van log files is noodsaaklik en tipies die eerste stap in die oplos van probleme met jou stelsel.';
+$lang['log_viewer_app_name'] = 'Meld Viewer';
+$lang['log_viewer_display'] = 'Wys';
+$lang['log_viewer_entry'] = 'Meld Entry';
+$lang['log_viewer_export_to_file'] = 'Uitvoer na lêer';
+$lang['log_viewer_file'] = 'log lêer';
+$lang['log_viewer_filter'] = 'filter';
+$lang['log_viewer_invalid_log_file'] = 'Ongeldige log lêer.';
 $lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_result_set_too_big'] = 'Die resultaat stel is groter as die maksimum wat kan vertoon. Gebruik asseblief die filter om jou resultate te verfyn.';
+$lang['log_viewer_show_full_line'] = 'Wys Full Line';
