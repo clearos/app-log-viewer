c3cf85b083efc59b0d1b0e56b406f7e05d8230b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:54:16 +0000

    Add new files and directories and commit changes to local repository

--- language/lv/log_viewer_lang.php
+++ language/lv/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
+$lang['log_viewer_app_description'] = 'Log Viewer app sniedz tabulas veidā displejs visu sistēmas log failus. Apskatei un meklēšanai no log failus, ir būtiska, un parasti ir pirmais solis, lai traucējummeklēšanu problēmas ar jūsu sistēmas.';
+$lang['log_viewer_app_name'] = 'log Viewer';
+$lang['log_viewer_display'] = 'displejs';
+$lang['log_viewer_entry'] = 'log Entry';
+$lang['log_viewer_export_to_file'] = 'Eksports uz File';
+$lang['log_viewer_file'] = 'log failu';
+$lang['log_viewer_filter'] = 'filtrs';
+$lang['log_viewer_invalid_log_file'] = 'Nederīgs log failu.';
+$lang['log_viewer_logs'] = 'baļķi';
+$lang['log_viewer_result_set_too_big'] = 'Rezultāts, kas ir lielāks par maksimāli kas var parādīt. Lūdzu, izmantojiet filtru, lai uzlabotu savus rezultātus.';
 $lang['log_viewer_show_full_line'] = 'Show Full Line';
