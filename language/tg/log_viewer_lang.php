0c510dd03e4979e83007a24eaeadf65ba09b1e23
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:58:28 +0000

    Add new files and directories and commit changes to local repository

--- language/tg/log_viewer_lang.php
+++ language/tg/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'Дар барнома Русский Навид Намоиши tabular ҳамаи файлҳои системаи Сабти таъмин менамояд. Тамошои ва ҷустуҷӯи файлҳои Сабти муҳим ва маъмулан қадами аввал дар мушкилоти носозиҳои бо системаи шумо ҳастанд.';
+$lang['log_viewer_app_name'] = 'Русский Навид';
+$lang['log_viewer_display'] = 'намоиш';
+$lang['log_viewer_entry'] = 'Дохилшавӣ Русский';
+$lang['log_viewer_export_to_file'] = 'Содирот ба файл';
+$lang['log_viewer_file'] = 'Воридшавӣ File';
+$lang['log_viewer_filter'] = 'Филтр';
+$lang['log_viewer_invalid_log_file'] = 'Файли нодуруст аст.';
+$lang['log_viewer_logs'] = 'Гузоришҳо';
+$lang['log_viewer_result_set_too_big'] = 'Дар маҷмӯи натиҷа калонтар аз ҳадди ки мумкин аст нишон дода аст. Лутфан, филтр истифода Барои дақиқ натиҷаҳои кунед.';
+$lang['log_viewer_show_full_line'] = 'Нишон Хати Full';
