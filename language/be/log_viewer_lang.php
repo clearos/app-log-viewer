f131a7dbffda24b9df4b613b4ff1a6118de3a98b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:49:30 +0000

    Add new files and directories and commit changes to local repository

--- language/be/log_viewer_lang.php
+++ language/be/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'Дадатак Log Viewer забяспечвае таблічны выснова ўсіх файлаў сістэмнага часопіса. Прагляд і пошук файлаў часопісаў маюць важнае значэнне і, як правіла, першым крокам у ліквідацыі праблем з вашай сістэмай.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_display'] = 'дысплей';
+$lang['log_viewer_entry'] = 'Уваход Уваход';
+$lang['log_viewer_export_to_file'] = 'Экспарт у файл';
+$lang['log_viewer_file'] = 'часопіс';
+$lang['log_viewer_filter'] = 'фільтр';
+$lang['log_viewer_invalid_log_file'] = 'Няправільны файл часопіса.';
+$lang['log_viewer_logs'] = 'бярвёны';
+$lang['log_viewer_result_set_too_big'] = 'Набор вынікаў больш, чым максімум, які можа быць адлюстраваны. Калі ласка, выкарыстоўвайце фільтр, каб удакладніць вынікі.';
+$lang['log_viewer_show_full_line'] = 'Паказваць Поўныя лініі';
