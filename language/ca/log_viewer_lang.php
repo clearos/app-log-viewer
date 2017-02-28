4eaf63434780a725f3d0d1db36e4a7af1487a0b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:50:08 +0000

    Add new files and directories and commit changes to local repository

--- language/ca/log_viewer_lang.php
+++ language/ca/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'El visualitzador de registre proporciona una exhibició tabular de tots els fitxers de registre del sistema. La visualització i recerca de fitxers de registre són essencials i en general el primer pas en la solució de problemes amb el sistema.';
+$lang['log_viewer_app_name'] = 'Visor de registre';
+$lang['log_viewer_display'] = 'visualització';
+$lang['log_viewer_entry'] = 'L&#39;entrada de registre';
+$lang['log_viewer_export_to_file'] = 'Exporta a fitxer';
+$lang['log_viewer_file'] = 'fitxer de registre';
+$lang['log_viewer_filter'] = 'filtre';
+$lang['log_viewer_invalid_log_file'] = 'fitxer de registre vàlid.';
+$lang['log_viewer_logs'] = 'registres';
+$lang['log_viewer_result_set_too_big'] = 'El conjunt de resultats és més gran que el màxim que es pot mostrar. Utilitzeu el filtre per filtrar els resultats.';
+$lang['log_viewer_show_full_line'] = 'Mostra línia completa';
