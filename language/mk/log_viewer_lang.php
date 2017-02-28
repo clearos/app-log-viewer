90ab931aa243ea3c9c03f5033d61175a6e13a300
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:54:38 +0000

    Add new files and directories and commit changes to local repository

--- language/mk/log_viewer_lang.php
+++ language/mk/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'стан на логови обезбедува табеларен приказ на сите системски датотеки. Увид и пребарување на лог датотеки кои се од суштинско значење и обично првиот чекор во решавањето на проблемите со вашиот систем.';
+$lang['log_viewer_app_name'] = 'логови';
+$lang['log_viewer_display'] = 'Покажи';
+$lang['log_viewer_entry'] = 'Пријавете се влез';
+$lang['log_viewer_export_to_file'] = 'Извоз во датотека';
+$lang['log_viewer_file'] = 'датотека за евиденција';
+$lang['log_viewer_filter'] = 'филтер';
+$lang['log_viewer_invalid_log_file'] = 'Невалидна датотека за евиденција.';
+$lang['log_viewer_logs'] = 'Дневници';
+$lang['log_viewer_result_set_too_big'] = 'сет резултатот е поголема од максималната големина што може да бидат прикажани. Ве молиме да користите филтер за да ја прилагодите на вашите резултати.';
+$lang['log_viewer_show_full_line'] = 'Прикажи целосна слика';
