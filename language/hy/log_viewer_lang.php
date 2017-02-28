11d330bff38c18c1fb12430491e40d379f2619dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:49:09 +0000

    Add new files and directories and commit changes to local repository

--- language/hy/log_viewer_lang.php
+++ language/hy/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'Գրանցումից հեռուստադիտողի ծրագիրը ապահովում է աղյուսակային ցուցադրման բոլոր համակարգ մուտք ֆայլեր. Անձնագրի եւ փնտրում log ֆայլեր են էական եւ, որպես կանոն, առաջին քայլն է troubleshooting խնդիրների հետ ձեր համակարգը.';
+$lang['log_viewer_app_name'] = 'log հեռուստադիտողի';
+$lang['log_viewer_display'] = 'ցուցադրել';
+$lang['log_viewer_entry'] = 'log Entry';
+$lang['log_viewer_export_to_file'] = 'Արտահանել Պատկեր';
+$lang['log_viewer_file'] = 'Մուտք Պատկեր';
+$lang['log_viewer_filter'] = 'ֆիլտր';
+$lang['log_viewer_invalid_log_file'] = 'Անվավեր ֆայլը.';
+$lang['log_viewer_logs'] = 'Տեղեկամատյաններ';
+$lang['log_viewer_result_set_too_big'] = 'Արդյունքն հավաքածու է ավելի մեծ, քան առավելագույնը, որ կարող է դրսեւորվել. Խնդրում ենք օգտագործել զտիչ է զտել Ձեր արդյունքները:';
+$lang['log_viewer_show_full_line'] = 'Ցույց տալ Full Line';
