2730e5143d6f89c5f721518368ec1b6b82f8fcce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:59:47 +0000

    Add new files and directories and commit changes to local repository

--- language/yi/log_viewer_lang.php
+++ language/yi/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'דער קלאָץ וויוער אַפּ גיט אַ טאַבולאַר אַרויסווייַזן פון אַלע סיסטעם קלאָץ טעקעס. וויוינג און שאַרף פון קלאָץ טעקעס זענען יקערדיק און טיפּיקלי דער ערשטער שריט אין טראָובלעשאָאָטינג פּראָבלעמס מיט אייער סיסטעם.';
+$lang['log_viewer_app_name'] = 'קלאָץ וויוער';
+$lang['log_viewer_display'] = 'אַרויסווייַז';
+$lang['log_viewer_entry'] = 'קלאָץ פּאָזיציע';
+$lang['log_viewer_export_to_file'] = 'עקספּאָרט צו טעקע';
+$lang['log_viewer_file'] = 'קלאָץ טעקע';
+$lang['log_viewer_filter'] = 'פילטער';
+$lang['log_viewer_invalid_log_file'] = 'אינוואליד קלאָץ טעקע.';
+$lang['log_viewer_logs'] = 'לאָגס';
+$lang['log_viewer_result_set_too_big'] = 'דער רעזולטאַט שטעלן איז גרעסער ווי די מאַקסימום וואס קענען זיין געוויזן. ביטע נוצן די פילטער צו ראַפינירן דיין רעזולטאַטן.';
+$lang['log_viewer_show_full_line'] = 'ווייַזן גאַנץ ליניע';
