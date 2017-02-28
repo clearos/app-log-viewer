323dd9ffbe4b36205d925fc55224562c2f736972
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:54:23 +0000

    Add new files and directories and commit changes to local repository

--- language/mg/log_viewer_lang.php
+++ language/mg/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'Ny Log Viewer fampiharana manome tabular fanehoana rafitra rehetra vatan-kazo raki-daza. Mijery sy mikaroka ny antontan-taratasy ilaina loga ka matetika no dingana voalohany eo amin&#39;ny Fanamboarana olana amin&#39;ny rafitra.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
 $lang['log_viewer_display'] = 'Display';
 $lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
+$lang['log_viewer_export_to_file'] = 'Export ny File';
+$lang['log_viewer_file'] = 'hiditra File';
 $lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_invalid_log_file'] = 'Vatan-kazo tsy mety rakitra.';
+$lang['log_viewer_logs'] = 'hazo';
+$lang['log_viewer_result_set_too_big'] = 'Ny vokany napetraka dia lehibe kokoa noho ny ambony indrindra izay azo aseho. Mba mampiasa ny sivana mba hanadiovana ny valiny.';
+$lang['log_viewer_show_full_line'] = 'Show Full-dalana,';
