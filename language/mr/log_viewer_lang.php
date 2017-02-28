714b4b3c30c9ea65042fcec038d9d10a5152f7a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:55:03 +0000

    Add new files and directories and commit changes to local repository

--- language/mr/log_viewer_lang.php
+++ language/mr/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'दर्शक लॉग अनुप्रयोग सर्व लॉग फाइल्स तक्त्याच्या प्रदर्शन उपलब्ध आहे. पहात आणि लॉग फाइल्स शोध अत्यावश्यक व प्रणालीसह समस्या निवारण समस्या विशेषत: पहिली पायरी आहे.';
+$lang['log_viewer_app_name'] = 'दर्शक लॉग';
+$lang['log_viewer_display'] = 'प्रदर्शन';
+$lang['log_viewer_entry'] = 'लॉग प्रवेश';
+$lang['log_viewer_export_to_file'] = 'फाइल निर्यात';
+$lang['log_viewer_file'] = 'लॉग इन फाइल';
+$lang['log_viewer_filter'] = 'फिल्टर';
+$lang['log_viewer_invalid_log_file'] = 'अवैध लॉग फाईल नाही.';
+$lang['log_viewer_logs'] = 'नोंदी';
+$lang['log_viewer_result_set_too_big'] = 'निकाल सेट जास्तीत जास्त प्रदर्शित केले जाऊ शकतात पेक्षा मोठा आहे. कृपया आपले परिणाम परिष्कृत फिल्टर वापरा.';
+$lang['log_viewer_show_full_line'] = 'पूर्ण लाइन शो';
