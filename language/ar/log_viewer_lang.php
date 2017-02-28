e3f66f500b651dbaebe99c6f77ac235a31d5aaa5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:49:02 +0000

    Add new files and directories and commit changes to local repository

--- language/ar/log_viewer_lang.php
+++ language/ar/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'ويوفر التطبيق عارض دخول عرض جدولي جميع ملفات سجل النظام. مشاهدة والبحث في ملفات السجل ضرورية وعادة الخطوة الأولى في استكشاف مشاكل مع النظام الخاص بك.';
+$lang['log_viewer_app_name'] = 'سجل المشاهد';
+$lang['log_viewer_display'] = 'عرض';
+$lang['log_viewer_entry'] = 'دخول دخول';
+$lang['log_viewer_export_to_file'] = 'تصدير إلى ملف';
+$lang['log_viewer_file'] = 'ملف تسجيل';
+$lang['log_viewer_filter'] = 'منقي';
+$lang['log_viewer_invalid_log_file'] = 'ملف السجل غير صالح.';
+$lang['log_viewer_logs'] = 'الجذوع';
+$lang['log_viewer_result_set_too_big'] = 'مجموعة النتائج أكبر من الحد الأقصى الذي يمكن عرضها. الرجاء استخدام فلتر لتحسين النتائج.';
+$lang['log_viewer_show_full_line'] = 'مشاهدة سطر كامل';
