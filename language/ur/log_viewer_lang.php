7fdae41e7da03c312ec22553819496601b224386
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:59:12 +0000

    Add new files and directories and commit changes to local repository

--- language/ur/log_viewer_lang.php
+++ language/ur/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'لاگ ان کریں ناظرین کو اے پی پی کے تمام نظام لاگ ان فائلوں کی ایک ٹیبل ڈسپلے فراہم. دیکھنے اور لاگ فائلوں کا تلاش کرنا ضروری اور آپ کے نظام کے ساتھ خرابیوں کا سراغ لگانا مسائل میں عام طور پر پہلا قدم ہو گا.';
+$lang['log_viewer_app_name'] = 'لاگ ان کریں ناظرین';
+$lang['log_viewer_display'] = 'ڈسپلے';
+$lang['log_viewer_entry'] = 'لاگ کے اندراج';
+$lang['log_viewer_export_to_file'] = 'برآمد فائل کرنا';
+$lang['log_viewer_file'] = 'لاگ فائل';
+$lang['log_viewer_filter'] = 'فلٹر';
+$lang['log_viewer_invalid_log_file'] = 'غلط لاگ فائل.';
+$lang['log_viewer_logs'] = 'لاگز';
+$lang['log_viewer_result_set_too_big'] = 'نتیجہ سیٹ زیادہ سے زیادہ ظاہر کیا جا سکتا ہے کہ زیادہ بڑی ہے. آپ کے نتائج کو بہتر کرنے کے لئے فلٹر کا استعمال کریں.';
+$lang['log_viewer_show_full_line'] = 'دکھائیں مکمل لائن';
