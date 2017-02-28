5c7ee1d4a23ce96753b6dadb9d4075b7655882ff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:56:59 +0000

    Add new files and directories and commit changes to local repository

--- language/sk/log_viewer_lang.php
+++ language/sk/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'The Log Viewer aplikácia poskytuje tabuľkové zobrazenie všetkých súborov denníka systému. Prehliadanie a vyhľadávanie súborov denníka sú nevyhnutné a zvyčajne prvý krok pri riešení problémov s týmto systémom.';
+$lang['log_viewer_app_name'] = 'prehliadač protokolov';
+$lang['log_viewer_display'] = 'zobraziť';
+$lang['log_viewer_entry'] = 'log Entry';
+$lang['log_viewer_export_to_file'] = 'Export do súboru';
+$lang['log_viewer_file'] = 'log súbor';
+$lang['log_viewer_filter'] = 'filter';
+$lang['log_viewer_invalid_log_file'] = 'Neplatný súbor denníka.';
+$lang['log_viewer_logs'] = 'protokoly';
+$lang['log_viewer_result_set_too_big'] = 'Sada výsledkov je väčší ako maximum, ktoré je možné zobraziť. Použite filter vylepšiť svoje výsledky.';
+$lang['log_viewer_show_full_line'] = 'Show plnou čiarou';
