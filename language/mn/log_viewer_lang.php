ebfa7f9715debe1ecaf8090dbd081323204df759
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:54:54 +0000

    Add new files and directories and commit changes to local repository

--- language/mn/log_viewer_lang.php
+++ language/mn/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
+$lang['log_viewer_app_description'] = 'Нэвтрэх Viewer програм нь бүх системийн бүртгэлийн файлууд нь хүснэгтэн дэлгэцийг олгодог. Хувийн болон бүртгэлийн файлуудыг хайх нь чухал бөгөөд таны систем алдааг олж засварлах нь асуудлууд нь ихэвчлэн эхний алхам юм.';
+$lang['log_viewer_app_name'] = 'нэвтрэх Viewer';
+$lang['log_viewer_display'] = 'Дэлгэц';
+$lang['log_viewer_entry'] = 'нэвтрэх төгөлдөр болох';
+$lang['log_viewer_export_to_file'] = 'Экспортын Файл';
+$lang['log_viewer_file'] = 'Файл Log';
 $lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_invalid_log_file'] = 'Буруу бүртгэлийн файл.';
+$lang['log_viewer_logs'] = 'бичиглэлээр';
+$lang['log_viewer_result_set_too_big'] = 'үр дүн тавих гарч болно дээд тал нь илүү том юм. Таны үр дүнг сайжруулах шүүлтүүр ашиглана уу.';
+$lang['log_viewer_show_full_line'] = 'Харуулах бүрэн шугам';
