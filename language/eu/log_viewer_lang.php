e805fc9145365c3fc76bc69163f24475051618d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:49:23 +0000

    Add new files and directories and commit changes to local repository

--- language/eu/log_viewer_lang.php
+++ language/eu/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
+$lang['log_viewer_app_description'] = 'Egunkarien ikustailea aplikazioa sistema log fitxategiak guztien erakusketa tabular bat eskaintzen du. Ikustea eta log fitxategiak bilatzen ezinbestekoa eta normalean zure sistemarekin Arazoak arazo lehen urratsa dira.';
+$lang['log_viewer_app_name'] = 'egunkarien ikustailea';
 $lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
+$lang['log_viewer_entry'] = 'Saioa Entry';
+$lang['log_viewer_export_to_file'] = 'Esportatu fitxategira';
+$lang['log_viewer_file'] = 'Egunkari fitxategia';
 $lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_invalid_log_file'] = 'Egunkari fitxategia baliogabea.';
+$lang['log_viewer_logs'] = 'Agerraldiak';
+$lang['log_viewer_result_set_too_big'] = 'emaitza multzo gehienezko erakus daitekeen baino handiagoa da. Mesedez, erabili iragazkia Emaitzak mugatzeko.';
+$lang['log_viewer_show_full_line'] = 'Erakutsi Full Line';
