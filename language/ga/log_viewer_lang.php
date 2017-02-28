a5dd314366552e1df3c9cf0f5ddb072f64171d6d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:52:39 +0000

    Add new files and directories and commit changes to local repository

--- language/ga/log_viewer_lang.php
+++ language/ga/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'Soláthraíonn an app Log Viewer taispeáint táblacha de gach comhad logáil isteach córas. Ag breathnú ar agus a chuardach ar comhaid a logáil riachtanach agus de ghnáth an chéad chéim i fadhbanna fabhtcheartaithe le do chóras.';
+$lang['log_viewer_app_name'] = 'Logáil Amharcán';
+$lang['log_viewer_display'] = 'Taispeáin';
+$lang['log_viewer_entry'] = 'Logáil Iontráil';
+$lang['log_viewer_export_to_file'] = 'Easpórtáil go Comhad';
+$lang['log_viewer_file'] = 'logáil Comhad';
+$lang['log_viewer_filter'] = 'scagaire';
+$lang['log_viewer_invalid_log_file'] = 'comhad loga neamhbhailí.';
+$lang['log_viewer_logs'] = 'logs';
+$lang['log_viewer_result_set_too_big'] = 'Is é an tacar toradh níos mó ná an t-uasmhéid is féidir a chur ar taispeáint. Bain úsáid as an scagaire a bheachtú do chuid torthaí.';
+$lang['log_viewer_show_full_line'] = 'Taispeáin Line Iomlán';
