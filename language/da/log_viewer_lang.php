02723c2271fab4f01742bc8463c2e3ff500e9a53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:50:48 +0000

    Add new files and directories and commit changes to local repository

--- language/da/log_viewer_lang.php
+++ language/da/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'The Log Viewer app giver en tabellarisk visning af alle systemets logfiler. Visning og søgning af logfiler er væsentlige og typisk det første skridt i fejlfinding af problemer med systemet.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
+$lang['log_viewer_display'] = 'Skærm';
+$lang['log_viewer_entry'] = 'Log indtastning';
+$lang['log_viewer_export_to_file'] = 'Eksport til fil';
+$lang['log_viewer_file'] = 'Logfil';
 $lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
+$lang['log_viewer_invalid_log_file'] = 'Ugyldig logfil.';
 $lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_result_set_too_big'] = 'Resultatet sæt er større end det maksimale, der kan vises. Brug venligst filter til at forfine dine resultater.';
+$lang['log_viewer_show_full_line'] = 'Vis fuld linje';
