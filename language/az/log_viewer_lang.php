dcda476207dee1c3e0ac35bc1816296d5f16856d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:49:16 +0000

    Add new files and directories and commit changes to local repository

--- language/az/log_viewer_lang.php
+++ language/az/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'Giriş Viewer app bütün sistem log faylları bir cədvəlli ekran təmin edir. Mə `lumat səhifəsi və log faylları axtarış əsas və sistem ilə giderme problemləri adətən ilk addımdır.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
+$lang['log_viewer_display'] = 'göstərmək';
+$lang['log_viewer_entry'] = 'Log Giriş';
+$lang['log_viewer_export_to_file'] = 'Export Fayl';
+$lang['log_viewer_file'] = 'Fayl Giriş';
+$lang['log_viewer_filter'] = 'Filtr';
+$lang['log_viewer_invalid_log_file'] = 'Yanlış log faylı.';
+$lang['log_viewer_logs'] = 'logs';
+$lang['log_viewer_result_set_too_big'] = 'Nəticədə set bilərsiniz maksimum daha böyükdür. nəticələri saflaşdırmaq filter istifadə edin.';
 $lang['log_viewer_show_full_line'] = 'Show Full Line';
