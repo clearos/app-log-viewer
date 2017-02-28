8b8f4353bd191809e0e27d3a4df33484d1e2bfb3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:58:55 +0000

    Add new files and directories and commit changes to local repository

--- language/tr/log_viewer_lang.php
+++ language/tr/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'Günlük Görüntüleyici uygulaması, tüm sistem log dosyalarını bir tablo görüntü sağlar. Görüntüleme ve günlük dosyalarının arama temel ve sistem ile ilgili sorunları gidermede genellikle ilk adımdır.';
+$lang['log_viewer_app_name'] = 'Günlük Görüntüleyici';
+$lang['log_viewer_display'] = 'ekran';
+$lang['log_viewer_entry'] = 'Giriş Giriş';
+$lang['log_viewer_export_to_file'] = 'İhracat Dosya';
+$lang['log_viewer_file'] = 'Log dosyası';
+$lang['log_viewer_filter'] = 'filtre';
+$lang['log_viewer_invalid_log_file'] = 'Geçersiz log dosyası.';
+$lang['log_viewer_logs'] = 'Kayıtlar';
+$lang['log_viewer_result_set_too_big'] = 'Sonuç kümesi görüntülenebilir maksimum değerden büyüktür. Sonuçlarınızı hassaslaştırmak için filtreyi kullanın.';
+$lang['log_viewer_show_full_line'] = 'Göster Tam Hattı';
