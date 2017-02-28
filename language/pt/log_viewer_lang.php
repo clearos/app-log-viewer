c2fb090c37d9348b2806d95cb5ff0ba2c9c03fde
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:56:13 +0000

    Add new files and directories and commit changes to local repository

--- language/pt/log_viewer_lang.php
+++ language/pt/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
+$lang['log_viewer_app_description'] = 'O aplicativo Log Viewer fornece uma exibição tabular de todos os arquivos de log do sistema. Visualizando e busca de arquivos de log são essenciais e, normalmente, o primeiro passo na resolução de problemas com o seu sistema.';
+$lang['log_viewer_app_name'] = 'Visualizador de log';
+$lang['log_viewer_display'] = 'Exibição';
+$lang['log_viewer_entry'] = 'Entrada log';
+$lang['log_viewer_export_to_file'] = 'Exportar para arquivo';
+$lang['log_viewer_file'] = 'Arquivo de log';
+$lang['log_viewer_filter'] = 'Filtro';
+$lang['log_viewer_invalid_log_file'] = 'arquivo de log inválido.';
 $lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_result_set_too_big'] = 'O conjunto de resultados é maior do que o máximo que pode ser exibido. Por favor, use o filtro para refinar os resultados.';
+$lang['log_viewer_show_full_line'] = 'Mostrar linha completa';
