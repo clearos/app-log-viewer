d72dd55cb755bd1a2713d0f281945da1c3b10870
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:52:55 +0000

    Add new files and directories and commit changes to local repository

--- language/is/log_viewer_lang.php
+++ language/is/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
+$lang['log_viewer_app_description'] = 'The Log Viewer app veitir tabular skjá allra kerfi skrá þig inn. Skoða og leita af log skrár eru nauðsynleg og yfirleitt fyrsta skrefið í bilanaleit vandamál með vélinni þinni.';
 $lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
+$lang['log_viewer_display'] = 'sýna';
 $lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_export_to_file'] = 'Flytja út í skrá';
+$lang['log_viewer_file'] = 'Annálaskráin';
+$lang['log_viewer_filter'] = 'Sía';
+$lang['log_viewer_invalid_log_file'] = 'Ógild annáll.';
+$lang['log_viewer_logs'] = 'logs';
+$lang['log_viewer_result_set_too_big'] = 'Niðurstaðan sett er stærri en hámarkið sem hægt er að birta. Vinsamlegast notaðu síu til að betrumbæta niðurstöður.';
+$lang['log_viewer_show_full_line'] = 'Sýna Full Line';
