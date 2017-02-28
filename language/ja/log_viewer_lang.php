6778f903e85ae9bf50258941cfee2ee7b9cf49a5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:53:10 +0000

    Add new files and directories and commit changes to local repository

--- language/ja/log_viewer_lang.php
+++ language/ja/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'ログビューアアプリは、すべてのシステムログファイルの表形式の表示を提供します。 表示とログファイルの検索が不可欠であり、典型的には、システムに関する問題のトラブルシューティングの最初のステップ。';
+$lang['log_viewer_app_name'] = 'ログビューア';
+$lang['log_viewer_display'] = '表示';
+$lang['log_viewer_entry'] = 'ログエントリ';
+$lang['log_viewer_export_to_file'] = 'ファイルへのエクスポート';
+$lang['log_viewer_file'] = 'ログファイル';
+$lang['log_viewer_filter'] = 'フィルタ';
+$lang['log_viewer_invalid_log_file'] = '無効なログファイル。';
+$lang['log_viewer_logs'] = 'ログ';
+$lang['log_viewer_result_set_too_big'] = '結果セットを表示することができる最大値よりも大きくなっています。 あなたの結果を絞り込むためのフィルタを使用してください。';
+$lang['log_viewer_show_full_line'] = 'フルラインを表示';
