94ac7239cdb70324cece50126e3ae898308bc99f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:51:11 +0000

    Add new files and directories and commit changes to local repository

--- language/fi/log_viewer_lang.php
+++ language/fi/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'Log Viewer App tarjoaa taulukkomuodossa näyttö kaikkien järjestelmän lokitiedostoja. Katseleminen ja lokitiedostoja ovat välttämättömiä ja yleensä ensimmäinen askel vianmääritys järjestelmän vikoja.';
+$lang['log_viewer_app_name'] = 'loki';
+$lang['log_viewer_display'] = 'Näyttö';
+$lang['log_viewer_entry'] = 'log Entry';
+$lang['log_viewer_export_to_file'] = 'Vie tiedostoon';
+$lang['log_viewer_file'] = 'Loki tiedosto';
+$lang['log_viewer_filter'] = 'Suodattaa';
+$lang['log_viewer_invalid_log_file'] = 'Virheellinen lokitiedosto.';
+$lang['log_viewer_logs'] = 'lokit';
+$lang['log_viewer_result_set_too_big'] = 'Tuloksena joukko on suurempi kuin enimmäismäärä, joka voidaan näyttää. Käytä suodatinta tarkentaa tuloksia.';
+$lang['log_viewer_show_full_line'] = 'Näytä koko Line';
