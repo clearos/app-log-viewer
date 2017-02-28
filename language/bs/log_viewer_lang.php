42633a73ff051c68f4a17c84a1f3f9a56abf48fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:49:45 +0000

    Add new files and directories and commit changes to local repository

--- language/bs/log_viewer_lang.php
+++ language/bs/log_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['log_viewer_app_description'] = ' ';
-$lang['log_viewer_app_name'] = 'Log Viewer';
-$lang['log_viewer_display'] = 'Display';
-$lang['log_viewer_entry'] = 'Log Entry';
-$lang['log_viewer_export_to_file'] = 'Export to File';
-$lang['log_viewer_file'] = 'Log File';
-$lang['log_viewer_filter'] = 'Filter';
-$lang['log_viewer_invalid_log_file'] = 'Invalid log file.';
-$lang['log_viewer_logs'] = 'Logs';
-$lang['log_viewer_result_set_too_big'] = ' ';
-$lang['log_viewer_show_full_line'] = 'Show Full Line';
+$lang['log_viewer_app_description'] = 'The Log Viewer aplikacija daje tabelarni prikaz svih datoteka sistema dnevnika. Pregled i pretraživanje datoteka dnevnika su od suštinskog značaja i obično prvi korak u rešavanje problema sa sistemom.';
+$lang['log_viewer_app_name'] = 'Prijavite Viewer';
+$lang['log_viewer_display'] = 'prikaz';
+$lang['log_viewer_entry'] = 'Prijavite Entry';
+$lang['log_viewer_export_to_file'] = 'Izvoz u File';
+$lang['log_viewer_file'] = 'log datoteke';
+$lang['log_viewer_filter'] = 'filter';
+$lang['log_viewer_invalid_log_file'] = 'Nevažeći datoteke dnevnika.';
+$lang['log_viewer_logs'] = 'Trupci';
+$lang['log_viewer_result_set_too_big'] = 'Set Rezultat je veći od maksimalnog koje se mogu prikazati. Molimo koristite filter da suzite rezultate.';
+$lang['log_viewer_show_full_line'] = 'Pokaži Full Line';
